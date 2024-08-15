283d3a89255cbed04fe1d879b8652a981b939ce2
Ryan <ryanlirio2@gmail.com>
2024-08-14 22:07:13 -0300

    First project net

---
+++ atividades/aula01/Program.cs
@@ -0,0 +1,17 @@
+﻿// comentário de uma linha
+
+/*
+    comentário de 
+    multiplas linhas
+*/    
+
+// Declarando Variavel
+int number;
+//Atribuindo valor
+number = 10;
+
+//Declarando constante
+const int DAYS_IN_WEEK = 7;
+
+//Imprimindo spring concatenada
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias.");
